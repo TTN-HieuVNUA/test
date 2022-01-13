bea0a1c1d80477b17b2401ca585e72e57da181bf
TTN-HieuVNUA <64110911+TTN-HieuVNUA@users.noreply.github.com>
2022-01-13 15:33:17 +0700

    Add files via upload

---
+++ btl.cpp
@@ -0,0 +1,153 @@
+#include<iostream>
+#include<stdio.h>
+#include<fstream>
+#include<string.h>
+using namespace std;
+
+class Image
+{
+    private:
+        string name;
+        string resolution;
+        string location;
+        string photoday;
+        int numViews;
+    public:
+        Image();
+        Image(string name, string resolution, string location, string photoday);
+        void setNumViews();
+        int getNumViews();
+        string getLocation();
+        string getName();
+        void showInfor();
+};
+
+class List
+{
+    private:
+        struct node
+        {
+            Image infor;
+            node *link;
+        }*F;
+    public:
+        List();
+        ~List();
+        Image findAImageByName(string imageName);
+        Image findAImageByLocation(string location);
+        Image showAImageMaxView();
+        void disPlay();
+};
+
+//==== chuong trinh chinh
+int main()
+{
+
+    cout<<endl;
+    return 0;
+}
+//==== dinh nghia ham====
+Image::Image()
+{
+
+}
+
+Image::Image(string name, string resolution, string location, string photoday)
+{
+    this->name=name;
+    this->resolution=resolution;
+    this->location=location;
+    this->photoday=photoday;
+    this->numViews=0;
+}
+
+void Image::setNumViews()
+{
+    this->numViews+=1;
+}
+
+int Image::getNumViews()
+{
+    return this->numViews;
+}
+
+string Image::getLocation()
+{
+    return this->location;
+}
+
+string Image::getName()
+{
+    return this->name;
+}
+
+void Image::showInfor()
+{
+    cout<<"\nten: "<<name<<", do phan giai: "<<resolution<<", dia diem chup: "<<location<<", thoi gian chup: "<<photoday<<", view: "<<numViews;
+}
+
+List::List()
+{
+    F=NULL;
+}
+
+List::~List()
+{
+    
+}
+
+Image List::findAImageByName(string imageName)
+{
+    node *P = F;
+    while(P != NULL)
+    {
+        if(P->infor.getName().compare(imageName))
+        {
+            P->infor.setNumViews();
+            return P->infor;
+        }
+        P = P->link;
+    }
+    return P->infor;
+}
+
+Image List::findAImageByLocation(string location)
+{
+    node *P = F;
+    while(P != NULL)
+    {
+        if(P->infor.getLocation().compare(location))
+        {
+            return P->infor;
+        }
+        P=P->link;
+    }
+    return P->infor;
+}
+
+Image List::showAImageMaxView()
+{
+    int max = 0;
+    node *P = F;
+    Image img;
+    while(P != NULL)
+    {
+        if(P->infor.getNumViews()>max)
+        {
+            max = P->infor.getNumViews();
+            img = P->infor;
+        }
+        P = P->link;
+    }
+    return img;
+}
+
+void List::disPlay()
+{
+    node *P = F;
+    while(P != NULL)
+    {
+        P->infor.showInfor();
+        P=P->link;
+    }
+}
